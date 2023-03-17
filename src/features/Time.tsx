575102af5cf21842dd4e5f906718abf9d3eabbf8
anilwagle808 <anil.wagle808@gmail.com>
2023-03-17 21:44:27 +0545

    Made Own package of TimePicker

--- src/features/Time.tsx
+++ src/features/Time.tsx
@@ -1,25 +1,31 @@
 import React, { useState, useRef, useEffect } from "react";
 
 interface TimePickerProps {
-  currentTime:any,
+  currentTime: any;
 }
-const Time: React.FC<TimePickerProps> = ({
-  currentTime
-}) => {
+const Time: React.FC<TimePickerProps> = ({ currentTime }) => {
   const [meridian, setMeridian] = useState({
-    aMeri: true,
-    pMeri: false,
+    aMeri: false,
+    pMeri: true,
   });
-  const [activeIndex,setActiveIndex] = useState(-1);
-  const [displayPicker,setDisplayPicker] = useState(false);
-  const [selectedTime, setSelectedTime] = useState('12:00');
+  const [activeIndex, setActiveIndex] = useState(21);
+  const [displayPicker, setDisplayPicker] = useState(false);
+  const [selectedTime, setSelectedTime] = useState("21:00");
   const timeContainerRef = useRef<HTMLDivElement>(null);
-  const timeArray = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  const timeArray = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23,
+  ];
+  console.log(activeIndex);
 
-
-  const handleActiveClick =(item:number)=>{
+  const handleActiveClick = (item: number) => {
     setActiveIndex(item);
-  }
+    if (item < 10) {
+      setSelectedTime(`0${item}:00`);
+    } else if (item >= 10) {
+      setSelectedTime(`${item}:00`);
+    }
+  };
 
   const handleClickOutside = (event: MouseEvent) => {
     if (
@@ -30,106 +36,94 @@ const Time: React.FC<TimePickerProps> = ({
     }
   };
 
-  const handleTimePicker = (event:React.MouseEvent<HTMLInputElement>) => {
+  const handleTimePicker = (event: React.MouseEvent<HTMLInputElement>) => {
     event.preventDefault();
     setDisplayPicker(true);
   };
-  const handleTimeBoxClick = (elem: number) => {
-    let selectedTime;
-    if (elem == 12 && meridian.pMeri) {
-      selectedTime = "12:00";
-    } else if (elem < 12 && meridian.pMeri) {
-      selectedTime = `${(elem += 12)}:00`;
-    } else if (elem == 12 && meridian.aMeri) {
-      selectedTime = "00:00";
-    } else if (elem >= 10 && meridian.aMeri) {
-      selectedTime = `${elem}:00`;
-    } else {
-      selectedTime = `0${elem}:00`;
-    }
-    console.log(selectedTime);
-    setSelectedTime(selectedTime);
+
+  const handleAmPm = (e: React.MouseEvent<HTMLButtonElement>, hand: string) => {
+    e.preventDefault();
+    setMeridian((prev) => {
+      return {
+        aMeri: hand == "am" ? true : false,
+        pMeri: hand == "pm" ? true : false,
+      };
+    });
   };
+
   useEffect(() => {
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [timeContainerRef]);
+
   return (
     <>
       <input
-          type="time"
-          className="datetime"
-          min={currentTime}
-          onClick={handleTimePicker}
-          onChange={()=>setSelectedTime(selectedTime)}
-          value={selectedTime}
-        />
-    <div className="time_container" ref={timeContainerRef}>
-      <style>{`
+        type="time"
+        className="datetime"
+        min={currentTime}
+        onClick={handleTimePicker}
+        onChange={() => setSelectedTime(selectedTime)}
+        value={selectedTime}
+      />
+      <div className="time_container" ref={timeContainerRef}>
+        <style>{`
         .time_container{
-          display:${displayPicker ? 'block':'none'};
+          display:${displayPicker ? "block" : "none"};
         }
       `}</style>
-      <div className="internal_time_container">
-        <div className="am_pm_container">
-          <button
-            className={`time_button am_button ${
-              meridian.aMeri ? "am_pm_active" : null
-            }`}
-            onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
-              setMeridian((prev) => {
-                e.preventDefault();
-                return {
-                  aMeri: true,
-                  pMeri: false,
-                };
-              })
-            }
-          >
-            AM
-          </button>
-          <button
-            className={`time_button pm_button ${
-              meridian.pMeri ? "am_pm_active" : null
-            }`}
-            onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
-              setMeridian((prev) => {
-                e.preventDefault();
-                return {
-                  aMeri: false,
-                  pMeri: true,
-                };
-              })
-            }
-          >
-            PM
-          </button>
-        </div>
-        <div className="time_tabulation_container">
-          <div className="time_internal_tabulation_container">
-            <div className="time_data_tabulation">
-              {timeArray.map((elem, index) => {
-                return (
-                  <div
-                    className={`time_box time_box${index} ${index == activeIndex ? 'time_box_active' : null}`}
-                    key={index}
-                    onClick={() => {
-                      handleTimeBoxClick(elem);
-                      handleActiveClick(index)
-                      setDisplayPicker(false);
-                    }}
-                  >
-                    <p>{elem}:00</p>
-                  </div>
-                );
-              })}
+        <div className="internal_time_container">
+          <div className="am_pm_container">
+            <button
+              className={`time_button am_button ${
+                meridian.aMeri ? "am_pm_active" : null
+              }`}
+              onClick={(e): void => handleAmPm(e, "am")}
+            >
+              AM
+            </button>
+            <button
+              className={`time_button pm_button ${
+                meridian.pMeri ? "am_pm_active" : null
+              }`}
+              onClick={(e): void => handleAmPm(e, "pm")}
+            >
+              PM
+            </button>
+          </div>
+          <div className="time_tabulation_container">
+            <div className="time_internal_tabulation_container">
+              <div className="time_data_tabulation">
+                {timeArray.map((elem, index) => {
+                  return (
+                    <div
+                      className={`time_box ${
+                        !meridian.aMeri && index < 12 ? "time_box_am" : ""
+                      } ${
+                        !meridian.pMeri && index >= 12 ? "time_box_pm" : ""
+                      } time_box${index} ${
+                        index == activeIndex ? "time_box_active" : null
+                      }`}
+                      key={index}
+                      onClick={() => {
+                        handleActiveClick(index);
+                        setDisplayPicker(false);
+                      }}
+                    >
+                      <p>
+                        {elem > 12 ? elem - 12 : elem}
+                        :00
+                      </p>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
